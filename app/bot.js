cda9330d1c0ee8eb8bfc2c0d5ae0e92af11967b2
k03mad <k03.mad@gmail.com>
2021-11-25 21:07:56 +0300

    update checks

--- app/bot.js
+++ app/bot.js
@@ -5,22 +5,31 @@ const errorsHandler = require('./telegram/errors');
 const {iot} = require('@k03mad/utils');
 const {telegram: {allowedChats}} = require('../env');
 
+const MAX_LEN = 100;
+
 errorsHandler(bot);
 
 bot.on('text', async ({text, chat}) => {
-    const value = text.trim();
+    const value = text.trim().replace(/\n+/g, '. ');
+    const valueLen = value.length;
+
     const sendMsg = msg => bot.sendMessage(chat.id, msg);
 
     if (allowedChats.includes(chat.id)) {
-        if (value.length > 1000) {
-            await sendMsg('Сообщение слишком длинное');
-
-        } else if (value.length < 2) {
-            await sendMsg('Сообщение слишком короткое');
+        if (valueLen > MAX_LEN) {
+            await sendMsg(
+                'Сообщение слишком длинное'
+              + `\n\nСимволов: ${valueLen}`
+              + `\nМаксимум: ${MAX_LEN}`,
+            );
 
         } else {
-            await iot.send({value});
-            await sendMsg('Отправлено');
+            try {
+                await iot.send({value});
+                await sendMsg('✅');
+            } catch (err) {
+                await sendMsg(err.message);
+            }
         }
     }
 });
