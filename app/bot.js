6cadfdf100b7b76139be9738db15b2c837a4dd57
k03mad <k03.mad@gmail.com>
2021-11-26 14:31:56 +0300

    simplify

--- app/bot.js
+++ app/bot.js
@@ -3,8 +3,10 @@
 const bot = require('./telegram/config');
 const errorsHandler = require('./telegram/errors');
 const {iot} = require('@k03mad/utils');
-const {splitStrings, checkForIncorrectMessage} = require('./utils/data');
 const {telegram: {allowedChats}} = require('../env');
+
+const MIN_MESSAGE_LEN = 2;
+const MAX_MESSAGE_LEN = 100;
 
 errorsHandler(bot);
 
@@ -12,24 +14,22 @@ bot.on('text', async ({text, chat: {id}}) => {
     if (allowedChats.includes(id)) {
         const sendMsg = (msg, opts = {}) => bot.sendMessage(id, msg, opts);
 
-        const ttsArr = splitStrings(text);
-        const error = checkForIncorrectMessage(ttsArr);
-
-        if (error) {
-            return sendMsg(error);
+        if (text.length < MIN_MESSAGE_LEN) {
+            return sendMsg('Команда Алисе должна содержать не менее двух букв');
         }
 
-        for (const tts of ttsArr) {
-            try {
-                await iot.send({value: tts});
-            } catch (err) {
-                return sendMsg(err.message);
-            }
+        if (text.length > MAX_MESSAGE_LEN) {
+            return sendMsg('Команда Алисе должна быть не длиннее 100 символов');
+        }
 
+        try {
+            const {value} = await iot.send({value: text});
             await sendMsg(
-                `Отправлено:\n\n\`\`\`\n${tts}\n\`\`\``,
+                `Отправлено:\n\n\`\`\`\n${value}\n\`\`\``,
                 {parse_mode: 'Markdown'},
             );
+        } catch (err) {
+            await sendMsg(err.message);
         }
     }
 });
--- app/utils/const.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-    MAX_LINES_TEXT: 10,
-    MAX_MESSAGE_LEN: 100,
-};
